3f3cd61500c29464f9a275b87cf48bec1bf5d8b1
chelle tuerk <michelletuerk@gmail.com>
2016-10-18 17:54:36 -0600

    Beginning of linked list JS, HTML, CSS

---
+++ functions.js
@@ -0,0 +1,9 @@
+var $userTitleInput = $('.titleInput')
+var $userURLInput = $('.URLInput')
+var $enterButton = $('.enter')
+
+$('.enter').on('click', function() {
+  var title = $userTitleInput.val()
+  var URL = $userURLInput.val()
+
+})
