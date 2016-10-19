b4420ed3eca5c3d65dd1b70fdd900a789f547dd7
chelle tuerk <michelletuerk@gmail.com>
2016-10-18 18:53:42 -0600

    Got the title input field to work

--- functions.js
+++ functions.js
@@ -4,6 +4,10 @@ var $enterButton = $('.enter')
 
 $('.enter').on('click', function() {
   var title = $userTitleInput.val()
+  var $title = $('.bookmarkTitle')
   var URL = $userURLInput.val()
-
+  var $URL = $('.bookmarkURL')
+  $title.text(title)
+  $URL.text(URL)
+  console.log
 })
