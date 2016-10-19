d8822dec26bc211b11f3b406d6c125a6ff7ae5e0
chelle tuerk <michelletuerk@gmail.com>
2016-10-18 21:28:08 -0600

    Added 'read' toggle.class and working remove button

--- functions.js
+++ functions.js
@@ -1,17 +1,27 @@
-var $userTitleInput = $('.titleInput')
-var $userURLInput = $('.urlInput')
-var $enterButton = $('.enter')
+var $userTitleInput = $('.titleInput');
+var $userURLInput = $('.urlInput');
+var $enterButton = $('.enter');
 
 $('.enter').on('click', function() {
-  var title = $userTitleInput.val()
-  var $title = $('.bookmarkTitle')
+  var title = $userTitleInput.val();
+  var $title = $('.bookmarkTitle');
 
-  var url = $userURLInput.val()
-  var $url = $('.bookmarkURL')
+  var url = $userURLInput.val();
+  var $url = $('.bookmarkURL');
   //$url.text(url)
   //$title.text(title)
 
-  $userTitleInput.val("")
-  $userURLInput.val("")
-  $(".bookmarks").append("<div class='card'><h2 class='bookmarkTitle'>"+title+"</h2><p class='bookmarkURL'>"+url+"</p><button class='bookmarkRead'>Read</button><button class='bookmarkRemove'>Delete</button></div>")
-})
+  $userTitleInput.val("");
+  $userURLInput.val("");
+  $(".bookmarks").append("<div class='card'><h2 class='bookmarkTitle'>"+title+"</h2><p class='bookmarkURL'>"+url+"</p><button class='bookmarkRead'>Read</button><button class='bookmarkRemove'>Delete</button></div>");
+/*Event listener is being added to every 
+.bookmarkRead button everytime .enter is clicked. we want it to only add once*/
+  $('.bookmarkRead').on('click', function() {
+    console.log("event");
+    $(this).toggleClass('read');
+  });
+
+  $('.bookmarkRemove').on('click', function() {
+    $('.card').remove();
+  });
+});
